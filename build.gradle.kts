d3bb3482a405c29907bfa4dddd209dd953728bc6
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-07-13 07:25:04 +0530

    add MavenPublication in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.dokka") version "1.9.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("maven-publish")
 }
 
-group = "io.shubham0204"
-version = "0.1"
+group = "com.github.shubham0204"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -14,12 +15,19 @@ repositories {
 dependencies {
     implementation("org.jsoup:jsoup:1.17.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create( "release" , MavenPublication::class.java ) {
+            from(components["java"])
+            groupId = "com.github.shubham0204"
+            artifactId = "google_search_kotlin"
+            version = "0.0.1"
+        }
+    }
 }
